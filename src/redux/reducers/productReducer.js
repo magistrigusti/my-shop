4d9712b5d6042210612716a7d69d8580ec6b371f
Magistry <Magistrigusti@yandex.ru>
2023-12-08 18:33:14 +0200

    first commit

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  products: []
+};
+
+export const productsReducer = (state, {type, payload}) => {
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+
+      break;
+
+    default: 
+      break;
+  }
+}
